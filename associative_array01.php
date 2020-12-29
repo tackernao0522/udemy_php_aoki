7c0b77e3a8852024d8d0cae4fe3467c8aba8448d
takaki <cheap_trick_magic@yahoo.co.jp>
2020-12-29 15:56:29 +0900

    PHPの連想配列 その1

---
+++ associative_array01.php
@@ -0,0 +1,12 @@
+<?php
+$array_member = [
+  'name' => '本田',
+  'height' => 170,
+  'hobby' => 'サッカー',
+];
+
+echo $array_member['hobby'];
+
+echo '<pre>';
+echo var_dump($array_member);
+echo '</pre>';
