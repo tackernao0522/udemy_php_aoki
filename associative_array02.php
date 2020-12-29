a166850df273c4d9623f1a3426866d80073871a8
takaki <cheap_trick_magic@yahoo.co.jp>
2020-12-29 16:17:43 +0900

    PHPの連想配列 その3

---
+++ associative_array02.php
@@ -0,0 +1,46 @@
+<?php
+$array_member_2 = [
+  '本田' => [
+    'height' => 170,
+    'hobby' => 'サッカー',
+  ],
+  '香川' => [
+    'height' => 165,
+    'hobby' => 'サッカー',
+  ],
+];
+
+echo $array_member_2['香川']['height'];
+
+echo '<pre>';
+echo var_dump($array_member_2);
+echo '</pre>';
+
+$array_member_3 = [
+  '1kumi' => [
+    '本田' => [
+      'height' => 170,
+      'hobby' => 'サッカー',
+    ],
+    '香川' => [
+      'height' => 165,
+      'hobby' => 'サッカー',
+    ],
+  ],
+  '2kumi' => [
+    '長友' => [
+      'height' => 160,
+      'hobby' => 'サッカー',
+    ],
+    '乾' => [
+      'height' => 168,
+      'hobby' => 'サッカー',
+    ],
+  ],
+];
+
+echo $array_member_3['2kumi']['長友']['height'];
+
+echo '<pre>';
+echo var_dump($array_member_3);
+echo '</pre>';
