5f89bed9140ad2ca2694ccd94a1b3e0c3021d7fe
takaki <cheap_trick_magic@yahoo.co.jp>
2020-12-29 15:03:21 +0900

    PHPの変数 その2

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 // 変数 動的型付 Java Visual Basic 静的型付
 
-$test = 123;
+$test_1 = 123;
+$test_2 = 456;
+
+$test_3 = $test_1 . $test_2; // ピリオドで繋ぐと文字列になる
+// 先頭は英文字かアンダーバー
+
 // $test = 'テストです';
 
-var_dump($test);
+var_dump($test_3);
 // echo $test;
