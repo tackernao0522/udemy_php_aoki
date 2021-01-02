4761db9101d60360033fd330c78587a6d5ed51a2
takaki <cheap_trick_magic@yahoo.co.jp>
2021-01-02 13:39:05 +0900

    フォームの入力、確認、完了

---
+++ form2/input.php
@@ -0,0 +1,62 @@
+<?php
+// スーパーグローバル変数 phpの場合は9種類ある
+// 連想配列
+if (!empty($_POST)) { // 空ではなければ中身がが表示される
+  echo '<pre>';
+  var_dump($_POST); // 送信結果が表示される
+  echo '</pre>';
+}
+
+// 入力、確認、完了 input.php, confirm.php, thanks.php
+// input.php
+$pageFlag = 0;
+
+if (!empty($_POST['btn_confirm'])) {
+  $pageFlag = 1;
+}
+if (!empty($_POST['btn_submit'])) {
+  $pageFlag = 2;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form Lesson</title>
+</head>
+
+<body>
+  <?php if ($pageFlag === 1) : ?>
+    <form method="POST" action="input.php">
+      氏名
+      <?php echo $_POST['your_name']; ?>
+      <br>
+      メールアドレス
+      <?php echo $_POST['email']; ?>
+      <br>
+      <input type="submit" name="btn_submit" value="送信する">
+      <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+      <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+    </form>
+  <?php endif; ?>
+  <?php if ($pageFlag === 2) : ?>
+    送信が完了しました。
+  <?php endif; ?>
+
+  <?php if ($pageFlag === 0) : ?>
+    <form method="POST" action="input.php">
+      氏名
+      <input type="text" name="your_name">
+      <br>
+      メールアドレス
+      <input type="email" name="email">
+      <br>
+      <input type="submit" name="btn_confirm" value="確認する">
+    </form>
+  <?php endif; ?>
+</body>
+
+</html>
