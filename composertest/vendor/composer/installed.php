0b0f916458a9601972e99b7a8a7306e2b3149dec
takaki <cheap_trick_magic@yahoo.co.jp>
2021-01-06 14:40:47 +0900

    composerの実行

---
+++ composertest/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7331e1a68e2529792385366b25a23e944c4024fb',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7331e1a68e2529792385366b25a23e944c4024fb',
+    ),
+  ),
+);
